8be7a66f963b3ff9136989ac67a0ca7aa1165f98
Alfarabi Bolatbek <91556021+4lfach@users.noreply.github.com>
2022-03-29 11:38:17 +0600

    Add files via upload

---
+++ FckngFractals/Commands/UpdateCurrentViewModelCommand.cs
@@ -0,0 +1,45 @@
+﻿using FckngFractals.State;
+using FckngFractals.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FckngFractals.Commands
+{
+    public class UpdateCurrentViewModelCommand :CommandBase
+    {
+        private INavigator _navigator;
+
+        public UpdateCurrentViewModelCommand(INavigator navigator)
+        {
+            _navigator = navigator;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            if (parameter is ViewType)
+            {
+                ViewType viewType = (ViewType)parameter;
+
+                switch (viewType)
+                {
+                    case ViewType.BinaryTree:
+                        _navigator.CurrentViewModel = new BinaryTreeViewModel();
+                        break;
+                    case ViewType.Snowflake:
+                        _navigator.CurrentViewModel = new SnowflakeViewModel();
+                        break;
+                    case ViewType.Mandelbrodt:
+                        break;
+                    case ViewType.About:
+                        break;
+                    default: break;
+                }
+            }
+
+            base.Execute(parameter);
+        }
+    }
+}
